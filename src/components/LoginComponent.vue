5aff4160c663546ae3a6eba3fb9ff2a9ad1ef784
imferr <117535778+imferr@users.noreply.github.com>
2024-03-12 00:28:17 -0400

    created pages of logout

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
+        <div class="navbar">
+            <button class="general" @click="login">Log In</button>
+        </div>
         <h1>Login</h1>
-        <button>
-            <router-link to="/home">Home</router-link>
-        </button>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'LoginComponent',
+    methods: {
+        login() {
+            this.$router.go(-1);
+            this.$router.replace('/home');
+        },
+    },
+};
+</script>
+
