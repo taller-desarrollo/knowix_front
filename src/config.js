99b2bfa586864d6ae132fb1cc8ffeb7ed1748b0c
admin <admin@admins-MacBook-Pro.local>
2024-03-13 00:18:23 -0400

    feat: add axios config and minor changes

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export var environment = {
+    name: "knowix",
+    flavor: "dev",
+    backendUrl: "http://127.0.0.1:8080"
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
+import { backendInterceptor } from './shared/interceptors/backend.interceptor.js';
+
 const app = createApp(App);
 
 app.use(router);
+
+app.config.globalProperties.$axios = backendInterceptor;
 
 app.mount('#app');
