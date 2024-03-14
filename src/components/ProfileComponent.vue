d88b16a951c67e250f170ee1732119c0c3f4f66f
imferr <117535778+imferr@users.noreply.github.com>
2024-03-14 18:19:55 -0400

    fixing profile view

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -1,37 +1,43 @@
 <template>
     <NavBarComponent />
     <div class="container-perfil">
-        <div class="profile">
-            <div class="encabezado-perfil">
+        <div class="profile1">
+            <div class="edit">
                 <h1>Mis Datos</h1>
             </div>
-            <div class="img-auth">
-                <font-awesome-icon icon="user" class="photouser" />
-            </div>
-            <div class="formulario-perfil">
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" />
+            <div class="up-profile">
+                <div class="img-auth">
+                    <font-awesome-icon icon="user" class="photouser" />
+                </div>
+                <div class="nameemail">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" readonly />
 
-                <label for="correo">Correo:</label>
-                <input type="email" id="correo" />
+                    <label for="correo">Correo:</label>
+                    <input type="email" id="correo" readonly />
+
+                    <label for="ocupacion">Ocupación:</label>
+                    <input type="text" id="ocupacion" readonly />
+                </div>
             </div>
         </div>
-        <div class="profile">
-            <div class="edit">
-                <h2>Información adicional</h2>
-                <button class="editbutton" id="editar">Editar</button>
-            </div>
+        <div class="profile1">
             <div class="formulario-perfil">
-                <label for="ocupacion">Ocupación:</label>
-                <input type="text" id="ocupacion" />
-
-                <h3>Redes Sociales</h3>
-                <label for="facebook">Facebook:</label>
-                <input type="text" id="facebook" />
-                <label for="twitter">Twitter:</label>
-                <input type="text" id="twitter" />
-                <label for="instagram">Instagram:</label>
-                <input type="text" id="instagram" />
+                <div class="networks">
+                    <h2>Redes Sociales</h2>
+                    <button class="editbutton" id="editar">Editar</button>
+                </div>
+                <div class="social-links">
+                    <div class="social-link" id="facebook">
+                        <a :href="socialLinks.facebook"><img src="../assets/facebook.png" alt="Facebook"></a>
+                    </div>
+                    <div class="social-link" id="twitter">
+                        <a :href="socialLinks.twitter"><img src="../assets/x.png" alt="Twitter"></a>
+                    </div>
+                    <div class="social-link" id="instagram">
+                        <a :href="socialLinks.instagram"><img src="../assets/instagram.png" alt="Instagram"></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -51,8 +57,23 @@ export default {
         NavBarComponent,
         FontAwesomeIcon
     },
+    data() {
+        return {
+            socialLinks: JSON.parse(localStorage.getItem('socialLinks')) || {
+                facebook: '#',
+                twitter: '#',
+                instagram: '#',
+            },
+        };
+    },
+    methods: {
+        updateLinks(newLinks) {
+            this.socialLinks = { ...this.socialLinks, ...newLinks };
+            this.$forceUpdate(); // Fuerza la actualización para reflejar los cambios
+        },
+    },
     mounted() {
-        makeFieldsEditable();
+        makeFieldsEditable(this.updateLinks, this.socialLinks);
     }
 };
 </script>
--- src/scripts/editProfile.js
+++ src/scripts/editProfile.js
@@ -1,20 +1,51 @@
 import Swal from 'sweetalert2';
 
-export function makeFieldsEditable() {
-    const inputs = document.querySelectorAll('.formulario-perfil input');
-    inputs.forEach(input => {
-        input.setAttribute('readonly', true);
+export function makeFieldsEditable(updateLinksCallback, initialSocialLinks) {
+    const inputs = document.querySelectorAll('.formulario-perfil input, .social-input');
+    const socialLinks = document.querySelectorAll('.social-link');
+    const btnEditar = document.getElementById('editar');
+    let isEditing = false;
+    const socialLinksData = {};
+
+    inputs.forEach(input => input.setAttribute('readonly', true));
+
+    socialLinks.forEach(link => {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.className = 'social-input';
+        input.style.display = 'none';
+        link.appendChild(input);
     });
 
-    const makeInputsEditable = () => {
+    const toggleInputs = (show) => {
         inputs.forEach(input => {
-            input.removeAttribute('readonly');
+            if (show) {
+                input.removeAttribute('readonly');
+            } else {
+                input.setAttribute('readonly', true);
+            }
+        });
+        document.querySelectorAll('.social-input').forEach(input => {
+            input.style.display = show ? 'block' : 'none';
         });
     };
 
-    const btnEditar = document.getElementById('editar');
-    if (btnEditar) {
-        btnEditar.addEventListener('click', () => {
+    const updateSocialLinks = (show) => {
+        socialLinks.forEach(link => {
+            const input = link.querySelector('.social-input');
+            const a = link.querySelector('a') || document.createElement('a');
+            if (show) {
+                a.href = input.value || initialSocialLinks[link.id] || '#';
+                socialLinksData[link.id] = input.value;
+                link.insertBefore(a, link.firstChild);
+            } else {
+                input.value = a.href;
+            }
+        });
+    };
+
+    btnEditar.addEventListener('click', () => {
+        if (!isEditing) {
             Swal.fire({
                 title: '¿Estás seguro?',
                 text: "Quieres editar los campos",
@@ -26,9 +57,37 @@ export function makeFieldsEditable() {
                 confirmButtonText: 'Sí'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    makeInputsEditable();
+                    isEditing = true;
+                    btnEditar.textContent = 'Guardar';
+                    toggleInputs(true);
+                    updateSocialLinks(false);
                 }
             });
-        });
+        } else {
+            Swal.fire({
+                icon: 'question',
+                title: '¿Guardar los cambios?',
+                text: '¿Estás seguro de que quieres guardar los cambios?',
+                showDenyButton: true,
+                confirmButtonText: 'Guardar',
+                denyButtonText: `Cancelar`,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('Guardado!', '', 'success');
+                    isEditing = false;
+                    btnEditar.textContent = 'Editar';
+                    toggleInputs(false);
+                    updateSocialLinks(true);
+                    localStorage.setItem('socialLinks', JSON.stringify(socialLinksData));
+                    updateLinksCallback(socialLinksData);
+                }
+            });
+        }
+    });
+
+    const storedLinks = JSON.parse(localStorage.getItem('socialLinks'));
+    if (storedLinks) {
+        Object.assign(initialSocialLinks, storedLinks);
     }
+    updateSocialLinks(true);
 }
