42f663976db888399d5f9d49b8914bcfcc7af121
IsaiDiaz <jsoza057@gmail.com>
2024-03-16 20:05:21 -0400

    Implemented configurations to enhance app security with Keycloak integration

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
 import { backendInterceptor } from './shared/interceptors/backend.interceptor.js';
+import Keycloak from 'keycloak-js';
 
-const app = createApp(App);
+(async () => {
+  const keycloak = new Keycloak({
+    url: 'http://localhost:8080/',
+    realm: 'Knowix',
+    clientId: 'knowix_frontend'
+  });
 
-app.use(router);
+  try {
+    const authenticated = await keycloak.init({ onLoad: 'login-required' });
+    console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
+  } catch (error) {
+    console.error('Failed to initialize adapter:', error);
+  }
 
-app.config.globalProperties.$axios = backendInterceptor;
+  const app = createApp(App);
 
-app.mount('#app');
+  app.use(router);
+
+  // Hacer que Keycloak esté disponible globalmente en la aplicación Vue
+  app.config.globalProperties.$keycloak = keycloak;
+
+  app.config.globalProperties.$axios = backendInterceptor;
+
+  app.mount('#app');
+})();
