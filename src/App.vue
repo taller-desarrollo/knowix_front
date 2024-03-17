d4043659c3f8b8fca143113dbe441cd635954a25
IsaiDiaz <jsoza057@gmail.com>
2024-03-16 21:00:41 -0400

    Added logout functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
 export default {
   name: 'App',
+  components: {
+    Navbar
+  }
 };
 </script>
+
+<style>
+ * {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+ }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <router-link class="navbar-brand" to="/">Mi Aplicación</router-link>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <button class="btn btn-outline-danger" @click="logout">Cerrar sesión</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      async logout() {
+        try {
+          await this.$keycloak.logout();
+          console.log('Usuario desconectado correctamente');
+           this.$router.push('/');
+        } catch (error) {
+          console.error('Error al cerrar sesión:', error);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1000; 
+  }
+  
+  .navbar {
+    padding: 0 1rem; 
+  }
+  
+  .navbar-brand {
+    margin-right: auto;
+  }
+  
+  .navbar-nav {
+    margin-left: auto; 
+  }
+  </style>
+  
