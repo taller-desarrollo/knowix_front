5d6ea24e2cefbdaa32b6c5f070d2e6d31e42ac24
CamperoJose <jose.campero@ucb.edu.bo>
2024-05-08 17:48:07 -0400

    reverse

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import { createPinia } from 'pinia';
 import * as components from 'vuetify/components'; 
 import * as directives from 'vuetify/directives'; 
 
+import environment from './config.js';
+
 library.add(faPencilAlt);
 
 const vuetify = createVuetify({
@@ -27,12 +29,13 @@ const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/',
+  url: environment.keycloakUrl,
   realm: 'Knowix',
   clientId: 'knowix_frontend'
 });
 
 keycloak.init({ onLoad: 'check-sso' }).then(authenticated => {
+  console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
   app.config.globalProperties.$keycloak = keycloak;
   app.config.globalProperties.$axios = backendInterceptor;
   app.use(router(keycloak));
