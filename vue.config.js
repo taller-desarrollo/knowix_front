c69f02810300970314b3f42ad700f2c5be33e75d
IsaiDiaz <jsoza057@gmail.com>
2024-05-17 18:39:57 -0400

    Corrected

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === 'production' ? '/knowix_front/': '/' 
-  lalal
   publicPath: '/' 
 })
